39b6c840b58b59ea1b2e28b8d26ad9bdfcd340f7
immayurpanchal <mayurpanchal002@gmail.com>
2019-10-15 00:02:10 +0530

    Added Search view

--- src/js/index.js
+++ src/js/index.js
@@ -1,27 +1,30 @@
 import Search from './models/Search';
+import * as searchView from './views/searchView';
+import { elements } from './views/base';
 
 const state = {};
 
 const controlSearch = async () => {
     // 1. Get query from view 
-    const query = 'pizza';
+    const query = searchView.getInput();
 
     if(query){
         // 2. New Search object and Add to state
         state.search = new Search(query);
 
         // 3. Prepare UI for results
+        searchView.clearInput();
+        searchView.clearResults();
 
         // 4. Search for recipes
         await state.search.getResults();
 
         // 5. Render the UI for results 
-        console.log(state.search.result);
-        
+        searchView.renderResults(state.search.result);
     }
 }
 
-document.querySelector('.search').addEventListener('submit', e => {
+elements.searchForm.addEventListener('submit', e => {
     e.preventDefault();
     controlSearch();
 });
